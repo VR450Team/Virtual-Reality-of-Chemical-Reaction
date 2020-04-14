adce29ed23b6cd131e3294aa4290a27c8a1f0635
Andrew Bowen <andrewbowen54@gmail.com>
2020-04-13 20:48:40 -0500

    File Secelction
    Adding file selection notification when a file is selected

---
+++ Assets/Scripts/ButtonPressed.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonPressed : MonoBehaviour{
+    public Text buttonText;
+    public Text displayText;
+    public void DisplayMessage() {
+        displayText = GameObject.Find("Canvas/DisplayMessage").GetComponent<Text>();
+
+        displayText.text = "File Selected";
+    }
+
+    /*
+    public void DisplayMessage1() {
+        //buttonText = GameObject.Find("Canvas/ScrollView/Viewport/Content/Rection1/Text").GetComponent<TextMesh>();
+        //displayText = GameObject.Find("Canvas/DisplayMessage").GetComponent<Text>();
+
+        //displayText.text = "Selected: " + buttonText.text;
+        
+    }
+    public void DisplayMessage2() {
+        buttonText = GameObject.Find("Canvas/Viewport/Content/Rection2/Text").GetComponent<Text>();
+        displayText = GameObject.Find("Canvas/DisplayText").GetComponent<Text>();
+
+        displayText.text = "Selected: " + buttonText.text;
+    }
+    */
+}
