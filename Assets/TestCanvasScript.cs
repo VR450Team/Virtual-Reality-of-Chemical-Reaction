071e04ae8e43bfdc1cab252afe9f9f31b0a21853
Nicholas Schroeder <nschroeder131@outlook.com>
2020-04-17 23:11:21 -0500

    Some work done on buttons in FileSelect

--- Assets/TestCanvasScript.cs
+++ Assets/TestCanvasScript.cs
@@ -12,21 +12,22 @@ public class TestCanvasScript : MonoBehaviour
     void Start()
     {
         GameObject button;
-        Vector3 buttonPosition = new Vector3(-150, 0, 0);
-        foreach(string filePath in System.IO.Directory.GetFiles("Assets/Resources/"))
+        Vector3 buttonPosition = new Vector3(0, 0, 0);
+        foreach(string filePath in System.IO.Directory.GetFiles("Assets/Files/"))
 		{
+            // Some files will be .meta files so ignore those
             if (filePath.Substring(filePath.Length - 4) == ".xyz")
             {
-                Debug.Log(filePath);
+                //Debug.Log(filePath);
                 button = Instantiate(buttonPrefab, this.transform) as GameObject;
                 button.transform.localPosition = buttonPosition;
 
-                // The first 17 characters are "Assets/Resources/" and we don't want that in the button's text.
+                // The first 13 characters are "Assets/Files/" and we don't want that in the button's text.
                 // The second parameter is the length of the substring, and we need to set it to filePath.Length
-                // - 21 so we the ".xyz" at the end isn't included.
-                button.GetComponentInChildren<Text>().text = filePath.Substring(17, filePath.Length - 21);
+                // - 17 so we the ".xyz" at the end isn't included.
+                button.GetComponentInChildren<Text>().text = filePath.Substring(13, filePath.Length - 17);
                 button.GetComponent<Button>().onClick.AddListener(delegate { goToMainScene(filePath); });
-                buttonPosition.x += 150;
+                buttonPosition.y += 150;
             }
 		}
         
--- Assets/TestScrollViewScript.cs
+++ Assets/TestScrollViewScript.cs
@@ -4,18 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class FileSelectButtonActions : MonoBehaviour
+public class TestScrollViewScript : MonoBehaviour
 {
     public GameObject buttonPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        MainSceneScript.filePath = "nothing";
-
         GameObject button;
-        Vector3 buttonPosition = new Vector3(0, 0, 0);
-        Vector3 buttonSize = new Vector3(2, 2, 0);
+        Vector3 buttonPosition = new Vector3(0, -150, 0);
         foreach (string filePath in System.IO.Directory.GetFiles("Assets/Files/"))
         {
             // Some files will be .meta files so ignore those
@@ -24,21 +21,21 @@ public class FileSelectButtonActions : MonoBehaviour
                 //Debug.Log(filePath);
                 button = Instantiate(buttonPrefab, this.transform) as GameObject;
                 button.transform.localPosition = buttonPosition;
-                button.transform.localScale = buttonSize;
 
                 // The first 13 characters are "Assets/Files/" and we don't want that in the button's text.
                 // The second parameter is the length of the substring, and we need to set it to filePath.Length
                 // - 17 so we the ".xyz" at the end isn't included.
                 button.GetComponentInChildren<Text>().text = filePath.Substring(13, filePath.Length - 17);
-                button.GetComponent<Button>().onClick.AddListener(delegate { setFilePath(filePath); });
+                button.GetComponent<Button>().onClick.AddListener(delegate { goToMainScene(filePath); });
                 buttonPosition.y -= 50;
             }
         }
 
     }
 
-    public void setFilePath(string filePath)
-	{
+    void goToMainScene(string filePath)
+    {
         MainSceneScript.filePath = filePath;
-	}
+        SceneManager.LoadScene("MainScene");
+    }
 }
